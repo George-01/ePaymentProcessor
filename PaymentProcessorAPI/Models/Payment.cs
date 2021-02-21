ad5488216cd054bcb798d46ad25438e0eb6497c6
George Coker <cgeorge@axamansard.com>
2021-02-21 21:45:57 +0100

    created required models for thw application

---
+++ PaymentProcessorAPI/Models/Payment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentProcessorAPI.Models
+{
+    public class Payment
+    {
+        [Required]
+        [Key]
+        public long PaymentId { get; set; }
+
+        [Required]
+        public string CreditCardNumber { get; set; }
+
+        [Required]
+        public string CardHolder { get; set; }
+
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+
+
+        [Column(nameof(SecurityCode), TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        [Column(nameof(Amount), TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
+
+        [InverseProperty(nameof(PaymentState.Payment))]
+        public virtual ICollection<PaymentState> PaymentStates { get; set; }
+    }
+}
---
+++ PaymentProcessorAPI/Models/PaymentState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentProcessorAPI.Models
+{
+    public class PaymentState
+    {
+        [Required]
+        [Key]
+        public int PaymentStateId { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+
+        [Column(nameof(DateCreated), TypeName = "datetime")]
+        public DateTime DateCreated { get; set; }
+
+        public int PaymentId { get; set; }
+
+        [ForeignKey(nameof(PaymentId))]
+        public Payment Payment { get; set; }
+    }
+}
